8d9e52f5cca2598a0f05f5660a4d18ac9287478d
Vaibhav Tiwari <122672330+Vaibhav-crux@users.noreply.github.com>
2024-08-14 11:48:21 +0530

    Add files via upload

---
+++ src/lib/content/footer.ts
@@ -0,0 +1,6 @@
+import { FooterSectionType } from '@/lib/types/sections';
+
+export const footerSection: FooterSectionType = {
+  title: 'Built by Vaibha Tiwari',
+  link: 'https://github.com/Vaibhav-crux',
+};
