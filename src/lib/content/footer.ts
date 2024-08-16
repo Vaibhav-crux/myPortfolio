b787f9828a459eb0fe0acf8f80048d9448e546da
Vaibhav Tiwari <122672330+Vaibhav-crux@users.noreply.github.com>
2024-08-16 15:53:36 +0530

    Delete src directory

--- src/lib/content/footer.ts
+++
@@ -1,6 +0,0 @@
-import { FooterSectionType } from '@/lib/types/sections';
-
-export const footerSection: FooterSectionType = {
-  title: 'Built by Vaibha Tiwari',
-  link: 'https://github.com/Vaibhav-crux',
-};
