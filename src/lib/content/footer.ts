0b21d4d23b3f568e222ea44b17b88121c4521d69
Vaibhav Tiwari <122672330+Vaibhav-crux@users.noreply.github.com>
2024-08-16 20:24:30 +0530

    Add files via upload

---
+++ src/lib/content/footer.ts
@@ -0,0 +1,7 @@
+import { FooterSectionType } from '@/lib/types/sections';
+
+export const footerSection: FooterSectionType = {
+  title: 'Crafted with passion and a love for continuous learning.',
+  description: 'Driven by curiosity and a relentless pursuit of knowledge. Always eager to connect with like-minded innovators and creators. Let’s build something amazing together!',
+  link: 'https://www.linkedin.com/in/vaibhav-tiwari83/',
+};
